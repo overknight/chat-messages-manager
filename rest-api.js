2a88228966a59c1636000bf18bb4f0a608dfad5f
overknight <overlord4@mail.ru>
2025-07-25 01:10:08 +0000

    REST API Improvements

--- rest-api.js
+++ rest-api.js
@@ -1,30 +1,43 @@
-const chatMessagesFile = require("./chat-messages"),
+const { read: readFile, openSync, createReadStream } = require("node:fs"),
+  chatMessagesFile = require("./chat-messages"),
   responseHeaders = require("./response-headers"),
+  messageTimestamps = new Set(),
+  prepareMessageTimestamps = callback=>{
+    if (!messageTimestamps.size) {
+      chatMessagesFile.read((_,date)=>{
+        messageTimestamps.add(date)
+      }, callback)
+      return
+    }
+    callback()
+  },
   apiMethods = new Map([
-    ["msg-timestamps", socket=>{
-      socket.write(responseHeaders.plainText)
+    ["msg-timestamps", socket=>prepareMessageTimestamps(()=>{
       let output = ""
-      chatMessagesFile.read((_, date)=>{
-        output += date + "\n"
-      }, ()=>{
-        output = output.slice(0, -1)
-        if (!output.length)
-          output = "no messages"
-        socket.end(output)
-      })
-    }],
+      for (const timestamp of messageTimestamps)
+        output += timestamp + "\n"
+      output = output.slice(0, -1)
+      if (!output.length)
+        output = "no messages"
+      socket.end(`${responseHeaders.plainText}${output}`)
+    })],
     ["message", (socket, targetMessage)=>{
       targetMessage = Number(targetMessage)
-      let found = false
-      chatMessagesFile.read((message, messageTimestamp)=>{
-        if (messageTimestamp == targetMessage) {
-          socket.end(`${responseHeaders.plainText}${message}`)
-          found = true
+      prepareMessageTimestamps(()=>{
+        if (!messageTimestamps.has(targetMessage)) {
+          socket.end(responseHeaders.notFoundWithText(`ERROR: message with timestamp ${targetMessage} not found`))
+          return
         }
-        // метод read объекта chatMessagesFile не предусмотрен для поиска записей поэтому при каждом вызове метода REST API
-        // будет осуществляться проход по всему файлу msg-storage, даже если запись найдена в начале файла
-      }, ()=>{
-        if (!found) socket.end(responseHeaders.notFoundWithText(`ERROR: message with timestamp ${targetMessage} not found`))
+        const position = chatMessagesFile.getOffsetForMessageTimestamp(targetMessage),
+          fd = openSync("msg-storage")
+        readFile(fd,{position, length:8},(_, __, buffer)=>{
+          const length = buffer.readUInt16LE(),
+            start = position + 8,
+            end = start + length - 1,
+            readStream = createReadStream(null, {fd, start, end})
+          socket.write(responseHeaders.plainText)
+          readStream.pipe(socket)
+        })
       })
     }]
   ]),
