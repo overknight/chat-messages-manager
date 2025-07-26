ffa569c041b334f151d75e245293b1dfec65f9e6
overknight <overlord4@mail.ru>
2025-07-26 01:21:00 +0000

    Added REST API Reference page

--- http-server.js
+++ http-server.js
@@ -33,7 +33,17 @@ const { createServer } = require("node:net"),
       color: #007bff;
       text-decoration: none;
     }
-    a:hover { text-decoration: underline; }`
+    a:hover { text-decoration: underline; }
+    code {
+      background: #f1f3f4;
+      padding: 2px 6px;
+      border-radius: 3px;
+      font-family: monospace;
+    }
+    .param {
+      font-style: italic;
+      color: #d35400;
+    }`
   }),
   responseHeaders = require("./response-headers"),
   chatMessagesFile = require("./chat-messages"),
@@ -49,7 +59,7 @@ const socketDataHandler = function(data) {
   if (!headers.path) {
     socket.write(responseHeaders.html)
     let output = "\n  <div class=\"container\">\n    <h1>Chat Messages</h1>\n" +
-      "    <p><a href=\"/api/msg-timestamps\">Message timestamps</a></p>\n",
+      "    <p><a href=\"/api-reference\">REST API Methods</a></p>\n",
       messageCounter = 0
     chatMessagesFile.read((message, date)=>{
       output += `    <div class="details">\n      <h3>${dateFormat(date)}</h3>\n      ${htmlEscape(message)}\n    </div>\n`
@@ -57,6 +67,39 @@ const socketDataHandler = function(data) {
     }, ()=>{
       if (!messageCounter) output += "    <div class=\"info\"><p>No messages</p></div>\n"
       output += "  <\/div>\n"
+      html_page(socket, output)
+    })
+    return
+  }
+  if (headers.path == "api-reference") {
+    const timestamps = []
+    let output = `\n  <div class=\"container\">\n    <h1>REST API Methods</h1>
+    <section>
+      <p><strong>Message timestamps list:</strong></p>
+      <p><code>/api/msg-timestamps</code></p>
+    </section>
+    <section>
+      <p><strong>Read message content:</strong></p>
+      <p><code>/api/message/<span class="param">{message_timestamp}</span></code></p>
+    </section>
+    <section>
+      <p><strong>Delete message:</strong></p>
+      <p><code>/api/message/<span class="param">{message_timestamp}</span>/delete</code></p>
+    </section>
+    <section>
+      <p><strong>Hint:</strong></p>
+      <p>To get <code><span class="param">{message_timestamp}</span></code> use <code>/api/msg-timestamps</code> method</p>
+    </section>
+    <h2>Examples</h2>
+    <ul>
+      <li><a href="/api/msg-timestamps">/api/msg-timestamps</a></li>`
+    chatMessagesFile.read((_, date)=>{
+      timestamps.push(date)
+    }, ()=>{
+      socket.write(responseHeaders.html)
+      for (const timestamp of timestamps.slice(-3))
+        output += `\n      <li><a href="/api/message/${timestamp}">/api/message/${timestamp}</a></li>`
+      output += "\n    </ul>\n    <hr>\n    <p><a href=\"/\">← Back to the Main Page</a></p>\n  <\/div>\n"
       html_page(socket, output)
     })
     return
